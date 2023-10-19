62b329512112a3d15f3d91a209332388a228028d
pedrocasc <pedrocascao03@gmail.com>
2023-10-19 16:49:08 +0100

    quero matar o python, python maluco

--- Pergunta2.py
+++ Pergunta2.py
@@ -2,7 +2,7 @@ from MedoTotal import *
 from searchPlus import *
 
 def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
-    def search(problem, frontier):
+    def search(problem, frontier, optimal, verbose):
         resultado = Node(state=None, path_cost=infinity)
         max_mem = 0
         visitados = 0
@@ -10,21 +10,33 @@ def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
         frontier.append(Node(problem.initial))
         while frontier:
             node = frontier.pop()
+            if (verbose):
+                print('-'*21, "\n")
+                print(problem.display(node.state))
             visitados += 1
             if problem.goal_test(node.state):
+                if verbose:
+                    print("GGGGooooooallllll --------- com o custo:", node.path_cost)
                 finais += 1
                 if node.path_cost < resultado.path_cost:
+                    if (verbose):
+                        print("Di best goal até agora")
                     resultado = node
-            elif node.path_cost < resultado.path_cost:
-                temp = len(frontier)
+            else:
+                if verbose:
+                    print("Custo:", node.path_cost)
                 children = list(node.expand(problem))
                 if children:
                     children.reverse()
+                if optimal:
+                    children = list(filter(lambda c: c.path_cost < resultado.path_cost, children))
                 frontier.extend(children)
                 max_mem = max(max_mem, len(frontier))
+            # print("CHILDREN =", len(children))
+            # print("MEMÓRIA MAX=", max_mem)
         return (resultado, max_mem, visitados, finais)
 
-    resultado, max_mem, visitados, finais = search(problem, Stack())
+    resultado, max_mem, visitados, finais = search(problem, Stack(), optimal, verbose)
 
     return resultado, max_mem, visitados, finais
 
@@ -44,11 +56,11 @@ linha6= "= = = = = =\n"
 grelha=linha1+linha2+linha3+linha4+linha5+linha6
 mundoStandardx=parametros + "\n" + grelha
 gx=MedoTotal(mundoStandardx)
-
-resultado,max_mem,visitados,finais = depth_first_tree_search_all_count(gx)
 print('*'*20)
+resultado,max_mem,visitados,finais = depth_first_tree_search_all_count(gx,True,True)
+
 if resultado:
-    print("Solução Prof-total (árvore) com custo " + str(resultado.path_cost)+":")
+    print("Solução Prof-total (árvore) com custo", str(resultado.path_cost)+":")
     print(resultado.solution())
 else:
     print('\nSem Solução')
