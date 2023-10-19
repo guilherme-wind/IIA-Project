ea299b8efadd7aeb7c02a9ae0066fbae095db883
pedrocasc <pedrocascao03@gmail.com>
2023-10-19 19:54:14 +0100

    pergunta 2 feita

--- Pergunta2.py
+++ Pergunta2.py
@@ -2,44 +2,54 @@ from MedoTotal import *
 from searchPlus import *
 
 def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
+    
     def search(problem, frontier, optimal, verbose):
-        resultado = Node(state=None, path_cost=infinity)
+        
         max_mem = 0
         visitados = 0
         finais = 0
         frontier.append(Node(problem.initial))
+
+        if problem.goal_test(problem.initial):
+            resultado = Node(problem.initial)
+        else:
+            resultado = Node(state=None, path_cost=infinity)
+        
         while frontier:
             node = frontier.pop()
-            max_mem = max(max_mem, len(frontier))
-            if optimal and node.path_cost >= resultado.path_cost:
-                continue
+            visitados += 1
             if (verbose):
                 print('-'*21, "\n")
                 print(problem.display(node.state))
-            visitados += 1
-            if problem.goal_test(node.state):
-                if verbose:
-                    print("GGGGooooooallllll --------- com o custo:", node.path_cost)
-                finais += 1
-                if node.path_cost < resultado.path_cost:
-                    if (verbose):
-                        print("Di best goal até agora")
-                    resultado = node
-            else:
-                if verbose:
-                    print("Custo:", node.path_cost)
-                children = list(node.expand(problem))
-                if children:
-                    children.reverse()
-                if optimal:
-                    children = list(filter(lambda c: c.path_cost < resultado.path_cost, children))
-                frontier.extend(children)
-            # print("CHILDREN =", len(children))
-            # print("MEMÓRIA MAX=", max_mem)
+                print("Custo:", node.path_cost)
+            children = list(node.expand(problem))
+
+            for c in list(children):
+                if optimal and c.path_cost >= resultado.path_cost:
+                    children.remove(c)
+                elif problem.goal_test(c.state):
+                    visitados += 1
+                    if verbose:
+                        print('-'*21, "\n")
+                        print(problem.display(c.state))
+                        print("GGGGooooooallllll --------- com o custo:", c.path_cost)
+                    finais += 1
+                    if c.path_cost < resultado.path_cost:
+                        if (verbose):
+                            print("Di best goal até agora")
+                        resultado = c
+                    children.remove(c)
+
+            if children:
+                children.reverse()
+            frontier.extend(children)
+            max_mem = max(max_mem, len(frontier))
+
         return (resultado, max_mem, visitados, finais)
 
     resultado, max_mem, visitados, finais = search(problem, Stack(), optimal, verbose)
-
+    if resultado.path_cost == infinity:
+        resultado = None
     return resultado, max_mem, visitados, finais
 
 
@@ -58,11 +68,11 @@ linha6= "= = = = = =\n"
 grelha=linha1+linha2+linha3+linha4+linha5+linha6
 mundoStandardx=parametros + "\n" + grelha
 gx=MedoTotal(mundoStandardx)
-print('*'*20)
-resultado,max_mem,visitados,finais = depth_first_tree_search_all_count(gx,True,True)
 
+resultado,max_mem,visitados,finais = depth_first_tree_search_all_count(gx,False,True)
+print('*'*20)
 if resultado:
-    print("Solução Prof-total (árvore) com custo", str(resultado.path_cost)+":")
+    print("Solução Prof-total (árvore) com custo " + str(resultado.path_cost)+":")
     print(resultado.solution())
 else:
     print('\nSem Solução')
