462629943aa62e1d26ee84dc4dba3beeaad3fc6b
BuildTools <unconfigured@null.spigotmc.org>
2023-10-21 15:54:48 +0100

    praticamente quase

--- Pergunta3.py
+++ Pergunta3.py
@@ -12,26 +12,25 @@ def ida_star_graph_search_count(problem,f,verbose=False):
             if verbose:
                 print("\n" + str(node.state))
                 print("Cost:", node.path_cost, "f=", f(node))
-            expandidos+=1
-            if problem.goal_test(node.state):
+            expandidos += 1
+            if f(node) > threshold:
+                new_threshold = min(new_threshold, f(node))
                 if verbose:
-                    print("Goal found within cutoff!")
-                return (node, expandidos, new_threshold)
-            explored.add(node.state)            
-            children = node.expand(problem)
-            if children:
-                children.reverse()
-            for child in children:
-                if f(child) > threshold and child.state not in explored:
+                    print("Out of cutoff -- minimum out:", f(node))
+            elif problem.goal_test(node.state):
+                if f(node) <= threshold:
                     if verbose:
-                        print("\n" + str(child.state))
-                        print("Cost:", child.path_cost, "f=", f(child))
-                        print("Out of cutoff -- minimum out:", f(child))
-                    new_threshold = min(new_threshold, f(child))
-                    expandidos += 1
-                    explored.add(child.state)
-                elif child.state not in explored and child not in frontier and f(child) <= threshold:
-                    frontier.append(child)
+                        print("Goal found within cutoff!")
+                    return (node, expandidos, new_threshold)
+                elif verbose:
+                    print("Out of cutoff -- minimum out:", f(node))
+            explored.add(node.state)
+            children = node.expand(problem)
+            if children and f(node) <= threshold:
+                children.reverse()
+                for child in children:
+                    if child.state not in explored and child not in frontier:
+                        frontier.append(child)
         return (None, expandidos, new_threshold)
 
     threshold = f(Node(problem.initial))
@@ -52,12 +51,31 @@ def ida_star_graph_search_count(problem,f,verbose=False):
 
 
 
-s = ProblemaGrafo()
+
+parametros="T=23\nM=7\nP=8"
+linha1= "= = = = = = = = = =\n"
+linha2= "= @ . . . . * . . =\n"
+linha3= "= . = = = = . = . =\n"
+linha4= "= . = F * = . . . =\n"
+linha5= "= = = = . = . = . =\n"
+linha6= "= * = . . . . . . =\n"
+linha7= "= . = = = = . = = =\n"
+linha8= "= * . . . . . = . =\n"
+linha9= "= * . . . . . . . =\n"
+linha10="= = = = = = = = = =\n"
+grelha=linha1+linha2+linha3+linha4+linha5+linha6+linha7+linha8+linha9+linha10
+mundoStandardx=parametros + "\n" + grelha
+gx=MedoTotal(mundoStandardx)
 print('---------------- IDA* ----------------')
-f=lambda n: n.path_cost + s.h1(n)
-res_IDAstar,visitados=ida_star_graph_search_count(s,f,True)
+f=lambda n: n.path_cost + gx.minimal_h(n)
+res_IDAstar,visitados=ida_star_graph_search_count(gx,f)
 if res_IDAstar:
     print("\nSolução:",res_IDAstar.solution(),'com custo',res_IDAstar.path_cost)
 else:
     print('\nSem Solução')
 print('Visitados:',visitados)
+
+# ---------------- IDA* ----------------
+
+# Solução: ['S', 'N', 'E', 'E', 'E', 'E', 'E', 'S', 'S', 'S', 'S', 'W', 'W', 'N', 'N', 'S', 'S', 'E', 'E', 'E', 'E', 'N', 'N'] com custo 28
+# Visitados: 2342
